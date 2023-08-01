9e56154c94c5bfbd33938f3a94a3ff5ecc74930d
meeyi_zhao <meeyi_zhao@realsil.com.cn>
2023-08-01 15:07:51 +0800

    Wearable: split source code from HoneyGUI
    1)HoneyGUI commit ID: 75e6c8142f4609c4ec0f7dc0130fbc9e1e182b8b
    2)vglite
    3)rt-thread
    4)other module from wearable
    
    Signed-off-by: howie_wang <375048164@qq.com>
    Change-Id: I28e507ee63cf48192bdd6e020b8b70744ac8db0c

---
+++ script/ignore_check_file.py
@@ -0,0 +1,22 @@
+ignore_file_encoding = [
+    r"aliIOT/",
+    r"VGLite/",
+    r"tuya-ble-sdk/",
+    r"database/",
+    r"vcxx/",
+    r"rt-thread/",
+    r"miwear-sdk_0.2.13/"
+    r"music_player/app_playback.c"
+    r"music_player/mp3_parser.c"
+]
+ignore_file_check = [
+    r"aliIOT/",
+    r"VGLite/",
+    r"tuya-ble-sdk/",
+    r"database/",
+    r"vcxx/",
+    r"rt-thread/",
+    r"miwear-sdk_0.2.13/"
+    r"music_player/app_playback.c"
+    r"music_player/mp3_parser.c"
+]
