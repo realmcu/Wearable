3cbd6795c54f4d36b1b3cd7c1ee0e917dd228d00
howie_wang <howie_wang@realsil.com.cn>
2023-10-13 16:13:59 +0800

    Wearable: add button cb
    Signed-off-by: howie_wang <howie_wang@realsil.com.cn>
    Change-Id: I29f6a414d12f0b232ac2146b4559a0b7e8525f06

--- key_button/8772F/key_button_8772f.c
+++ key_button/8772F/key_button_8772f.c
@@ -21,13 +21,18 @@
 
 void (*key_button_wakeup_indicate)(void) = NULL;
 
+static void button_cb(void *this)
+{
+    DBG_DIRECT("button_cb");
+}
+
 
 void rtk_key_button_hal_set_indicate(void (*indicate)(void *))
 {
     drv_pin_mode(KEY_BUTTON, PIN_MODE_INPUT);
     drv_pin_attach_irq(KEY_BUTTON, PIN_IRQ_MODE_RISING_FALLING, indicate,
                        NULL);
-    drv_pin_irq_enable(KEY_BUTTON, PIN_IRQ_DISABLE);
+    drv_pin_irq_enable(KEY_BUTTON, PIN_IRQ_ENABLE);
 }
 
 void rtk_key_button_hal_int_config(bool enable)
@@ -81,10 +86,12 @@ static bool key_button_system_wakeup_dlps_check(void)
 void drv_key_button_dlps_init(void)
 {
     System_WakeUpPinEnable(KEY_BUTTON, PAD_WAKEUP_POL_LOW, PAD_WAKEUP_DEB_DISABLE);
+#ifdef RTK_HAL_DLPS
     drv_dlps_exit_cbacks_register("key", key_button_exit_dlps);
     drv_dlps_enter_cbacks_register("key", key_button_enter_dlps);
     drv_dlps_wakeup_cbacks_register("key", key_button_system_wakeup_dlps_check);
     drv_dlps_check_cbacks_register("key", key_button_allowed_enter_dlps_check);
+#endif
 }
 
 void drv_key_button_set_wakeup_indicate(void (*wakeup_ind))
@@ -113,5 +120,6 @@ void rtk_key_button_hal_init(void)
     drv_pin_mode(KEY_BUTTON, PIN_MODE_INPUT);
 
     drv_key_button_dlps_init();
+
 }
 
