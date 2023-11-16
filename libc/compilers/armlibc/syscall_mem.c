21cd32cc305ab5ee1bb76e89cdd3f9cd4e642ee9
xieyr <roy_xie@realsil.com.cn>
2023-11-16 17:33:28 +0800

    Wearable: fix mem api
    Change-Id: I7c0fe3599769b2af0d825e6e49f23d88780b1bbc

--- libc/compilers/armlibc/syscall_mem.c
+++ libc/compilers/armlibc/syscall_mem.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
-
+#include "mem_types.h"
+#include <os_mem.h>
 
 void *malloc(size_t n)
 {
-    return NULL;
+    void *ptr = (void *)os_mem_alloc(RAM_TYPE_DATA_ON, n);
+    if (ptr == NULL)
+    {
+//        DBG_DIRECT("[malloc]!!!!!!!!!!MALLOC FAIL!!!!!!!!!");
+    }
+
+    return ptr;
 }
 
 void *calloc(size_t n, size_t size)
 {
-    return NULL;
+    return (void *)os_mem_zalloc(RAM_TYPE_DATA_ON, n * size);
 }
 
 void *realloc(void *ptr, size_t n)
 {
-    return NULL;
+    void *p = (void *)os_mem_alloc(RAM_TYPE_DATA_ON, n);
+    if (ptr)
+    {
+        memcpy(p, ptr, n);
+        os_mem_free(ptr);
+    }
+    return p;
 }
 
 void free(void *ptr)
