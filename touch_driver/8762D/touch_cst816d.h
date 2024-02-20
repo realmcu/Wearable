83515601a95c822c3bcc6052576f1b4f1cddecf7
sienna_shen <sienna_shen@realsil.com.cn>
2024-02-20 10:35:26 +0800

    Wearable: add cst816d touch driver
    Change-Id: I2388aba99b8c5bbb6187400d4ea59a95176d8f47

---
+++ touch_driver/8762D/touch_cst816d.h
@@ -0,0 +1,34 @@
+/**
+*****************************************************************************************
+*     Copyright(c) 2023, Realtek Semiconductor Corporation. All rights reserved.
+*****************************************************************************************
+* @file    touch_cst816d.h
+* @brief   This file provides CST816D touch driver functions
+* @author
+* @date
+* @version v1.0
+* *************************************************************************************
+*/
+
+#ifndef __TOUCH_CST816D_H
+#define __TOUCH_CST816D_H
+
+#include "board.h"
+#include "stdint.h"
+#include "stdbool.h"
+
+#define TOUCH_CST816D_RST                                     P2_3
+#define TOUCH_CST816D_INT                                     P0_4
+#define TOUCH_CST816D_SCL                                     P0_0
+#define TOUCH_CST816D_SDA                                     P0_2
+
+#define TOUCH_WORK_MODE_SLAVE_ADDRESS             0x15
+#define TOUCH_UPGRADE_MODE_SLAVE_ADDRESS          0x6a
+
+void rtk_touch_hal_init(void);
+bool rtk_touch_hal_read_all(uint16_t *x, uint16_t *y, bool *pressing);
+void rtk_touch_hal_int_config(bool enable);
+void rtk_touch_hal_set_indicate(void (*indicate)(void *));
+
+#endif // __TOUCH_CST816T_H
+
